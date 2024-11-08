404b6af15892de161611b28f9d1e6d01160c861e
Nikita <fomin.nv@phystech.edu>
2024-11-08 19:11:22 +0300

    table component

---
+++ resources/js/components/Index.vue
@@ -0,0 +1,22 @@
+<template>
+    <TableComponent />
+</template>
+
+<script lang="ts">
+    interface IRowData {
+        name: string,
+        amount: int,
+        source: string,
+        lazer: int,
+        welding: int,
+        electricity: int
+    };
+
+    import TableComponent from './Table.vue';
+    export default {
+        props: {
+            columns: Array<string>,
+            row_data: Array<IRowData>
+        }
+    };
+</script>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -14,7 +14,7 @@
     </head>
     <body>
         <div id="app">
-            <example-component />
+            <main-component />
         </div>
     </body>
 </html>
